a086741bd69829819b696a81d11ebab2627a836c
Yasmany Cubela Medina <yasmanycm@gmail.com>
2013-07-03 00:40:55 -0430

    updating composer

--- BITGoogleBundle.php
+++ BITGoogleBundle.php
@@ -8,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace BIT\GoogleBundle;
 use BIT\GoogleBundle\DependencyInjection\Security\Factory\GoogleFactory;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
