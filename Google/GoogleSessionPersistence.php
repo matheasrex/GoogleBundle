9d24e383edb116f2a956055e873026c75cc72b28
gyurusm <mate.gyurus@webbandflow.co.uk>
2013-07-30 14:16:51 +0200

    enable absolute scopes for google connection

--- Google/GoogleSessionPersistence.php
+++ Google/GoogleSessionPersistence.php
@@ -21,41 +21,45 @@ use Google_Oauth2Service as Service;
 class GoogleSessionPersistence extends Google_Client
 {
   const PREFIX = '_bit_google_';
-  
+
   private $oauth;
   private $session;
   private $prefix;
   protected static $kSupportedKeys = array( 'state', 'code', 'access_token', 'user_id' );
-  
+
   public function __construct( $config, Session $session, $prefix = self::PREFIX )
   {
     parent::__construct( $config );
-    
+
     $this->setApplicationName( $config[ "app_name" ] );
     $this->setClientId( $config[ "client_id" ] );
     $this->setClientSecret( $config[ "client_secret" ] );
     $this->setRedirectUri( $config[ "callback_url" ] );
-    
+
     $scopes = array( );
-    foreach ( $config[ "scopes" ] as $scope )
-      $scopes[ ] = "https://www.googleapis.com/auth/" . $scope;
-    
+    foreach ( $config[ "scopes" ] as $scope ) {
+      if (!strstr($scope, '\\')) {
+        $scope = "https://www.googleapis.com/auth/" . $scope;
+      }
+      $scopes[ ] = $scope;
+    }
+
     $this->setScopes( $scopes );
     $this->setState( $config[ "state" ] );
     $this->setAccessType( $config[ "access_type" ] );
     $this->setApprovalPrompt( $config[ "approval_prompt" ] );
     $this->oauth = new Service( $this);
-    
+
     $this->session = $session;
     $this->prefix = $prefix;
     $this->session->start( );
   }
-  
+
   public function getOAuth( )
   {
     return $this->oauth;
   }
-  
+
   /**
    * Stores the given ($key, $value) pair, so that future calls to
    * getPersistentData($key) return $value. This call may be in another request.
@@ -65,7 +69,7 @@ class GoogleSessionPersistence extends Google_Client
    *
    * @return void
    */
-  
+
   protected function setPersistentData( $key, $value )
   {
     if ( !in_array( $key, self::$kSupportedKeys ) )
@@ -73,10 +77,10 @@ class GoogleSessionPersistence extends Google_Client
       self::errorLog( 'Unsupported key passed to setPersistentData.' );
       return;
     }
-    
+
     $this->session->set( $this->constructSessionVariableName( $key ), $value );
   }
-  
+
   /**
    * Get the data for $key
    *
@@ -85,7 +89,7 @@ class GoogleSessionPersistence extends Google_Client
    *
    * @return mixed
    */
-  
+
   protected function getPersistentData( $key, $default = false )
   {
     if ( !in_array( $key, self::$kSupportedKeys ) )
@@ -93,22 +97,22 @@ class GoogleSessionPersistence extends Google_Client
       self::errorLog( 'Unsupported key passed to getPersistentData.' );
       return $default;
     }
-    
+
     $sessionVariableName = $this->constructSessionVariableName( $key );
     if ( $this->session->has( $sessionVariableName ) )
       return $this->session->get( $sessionVariableName );
-    
+
     return $default;
-    
+
   }
-  
+
   /**
    * Clear the data with $key from the persistent storage
    *
    * @param string $key
    * @return void
    */
-  
+
   protected function clearPersistentData( $key )
   {
     if ( !in_array( $key, self::$kSupportedKeys ) )
@@ -116,27 +120,27 @@ class GoogleSessionPersistence extends Google_Client
       self::errorLog( 'Unsupported key passed to clearPersistentData.' );
       return;
     }
-    
+
     $this->session->remove( $this->constructSessionVariableName( $key ) );
   }
-  
+
   /**
    * Clear all data from the persistent storage
    *
    * @return void
    */
-  
+
   protected function clearAllPersistentData( )
   {
     foreach ( $this->session->all( ) as $k => $v )
     {
       if ( 0 !== strpos( $k, $this->prefix ) )
         continue;
-      
+
       $this->session->remove( $k );
     }
   }
-  
+
   protected function constructSessionVariableName( $key )
   {
     return $this->prefix . implode( '_', array( 'g', $this->getAppId( ), $key, ) );
